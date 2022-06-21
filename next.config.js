7819eecf8eca9d9dcb697a5c230c3e0147c55501
Harsh Agrawal <itsharshag@gmail.com>
2022-06-21 14:31:49 +0530

    Cleanup next.js config file

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-const withPWA = require('next-pwa');
-
 module.exports =
-// withPWA(
 {
   reactStrictMode: true,
   pageExtensions: ['jsx', 'js', 'tsx', 'ts'],
-  // pwa: {
-  //   dest: "public",
-  //   register: true,
-  //   skipWaiting: true
-  // },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(new webpack.DefinePlugin({
       IN_BROWSER: true,
@@ -20,4 +12,3 @@ module.exports =
     ANNUAL_SUBSCRIPTION_RAZORPAY_LINK: process.env.ANNUAL_SUBSCRIPTION_RAZORPAY_LINK
   }
 }
-// )
